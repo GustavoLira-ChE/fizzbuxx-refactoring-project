7096209d8a5a8a0fa4c5f04162a6443ba94064c5
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-28 11:41:20 -0500

    fix: code test to satisfy the requirements

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,28 +1,31 @@
+import Reader from './../../lib/utils/Reader';
+import ExplorerService from './../../lib/services/ExplorerService';
 
 describe("ExplorerService class test", () => {
     test("filterByMission function return a list of object that contain a specified property value", () => {
         //arrange and act
-        const explorers = Reader.readJsonFile('./../../explorers.json')
+        const explorers = Reader.readJsonFile('explorers.json')
         const nodeExplorers = ExplorerService.filterByMission(explorers,'node')
         //assert
-        expect(nodeExplorers[1]).toEqual(
-            expect.objectContainig({
-                mission: 'node'
-            })
-        )
+        for(let i = 0; i < nodeExplorers.length; i++){
+            expect(nodeExplorers[i].mission).toEqual('node')
+        }
     });
     test("getAmountOfExplorersByMission function return the number of objects inside an list that contain a specified property value", () => {
         //arrange and act
-        const explorers = Reader.readJsonFile('./../../explorers.json')
+        const explorers = Reader.readJsonFile('explorers.json')
         const nodeExplorersAmount = ExplorerService.getAmountOfExplorersByMission(explorers,'node')
         //assert
         expect(typeof nodeExplorersAmount).toBe('number')
     });
     test("getExplorersUsernamesByMission function return a list of username property value from a list of objects that contain a especified property value", () => {
-        const explorers = Reader.readJsonFile('./../../explorers.json')
+        const explorers = Reader.readJsonFile('explorers.json')
         const nodeExplorersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, 'node')
         //assert
-        const expected = /Woopa/
-        expect(nodeExplorersUsernames[1]).stringContaining(expected)
+        //Expected is setting as Woopa because all the explorers have the same base name 
+        const expected = /Woopa/ // /[A-Za-z0-9]/
+        for(let i = 0; i < nodeExplorersUsernames.length; i++){
+            expect(nodeExplorersUsernames[i]).toEqual(expect.stringMatching(expected))
+        }
     })
 })
