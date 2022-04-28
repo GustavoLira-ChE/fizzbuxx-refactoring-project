3275b66dc0d4d7ce5799b979246630443cbedd9c
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-28 17:05:18 -0500

    test: add final test to the rest of the functions

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -7,5 +7,18 @@ describe("ExplorerController class test", () => {
         for(let i = 0; i < nodeExplorers.length; i++){
             expect(nodeExplorers[i].hasOwnProperty('name','mission')).toBeTruthy()
         }
-    })
+    });
+    test("2. getExplorersUsernamesByMission function returns a list of explorers name in function of the mission value passed", () => {
+        //arrange and act
+        const nodeExplorersUsernames = ExplorerController.getExplorersUsernamesByMission('node');
+        //asserts
+        for(let i = 0; i < nodeExplorersUsernames.length; i++){
+            expect(typeof nodeExplorersUsernames[i]).toBe('string')
+        }
+    });
+    test("3. getExplorersAmountByMission function returns the number of explorer with the property mission value equals to the mission parameter passed", () => {
+        const amountOfNodeExplorers = ExplorerController.getExplorersAmountByMission('node');
+        //asserts
+        expect(typeof amountOfNodeExplorers).toBe('number')
+    });
 });
