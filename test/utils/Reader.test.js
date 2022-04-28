5f9619bf6c81ed8ee8b6cf5346b2d55a7fa53f3d
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-28 10:15:15 -0500

    fix: description test function

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,7 @@
 import Reader from './lib/utils/Reader'
 
 describe("Reader class test", () => {
-    test("", () => {
+    test("The class function return a list of objects", () => {
         //arrange and act
         const explorers = Reader.readJsonFile('./../../explorers.json')
         //assert
