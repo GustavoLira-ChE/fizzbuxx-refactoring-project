da62f18312e382d0950852e54ce423bcfe0d57e9
Gustavo Lira <gu.lirar23@gmail.com>
2022-04-28 12:11:40 -0500

    add: fizzbuzz validation property value

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,3 +1,5 @@
+import Reader from './../../lib/utils/Reader'
+import FizzbuzzService from '../../lib/services/FizzbuzzService';
 
 describe("FizzbuzzService class test", () => {
     test("applyValidationInExplorer function apply a validation over 'score' property and add a new property 'trick' in function of 'score' value", () => {
@@ -10,5 +12,18 @@ describe("FizzbuzzService class test", () => {
         for(let i = 0; i < explorers.length; i++){
             expect(explorers[i].hasOwnProperty('trick')).toBeTruthy();
         }
+        for(let i = 0; i < explorers.length; i++){
+            if(explorers[i].score % 3 === 0 && explorers[i].score % 5 === 0){
+                expect(explorers[i].trick).toBe('FIZZBUZZ')
+            } else if(explorers[i].score % 3 === 0){
+                expect(explorers[i].trick).toBe('FIZZ')
+            }
+            else if(explorers[i].score % 5 === 0){
+                expect(explorers[i].trick).toBe('BUZZ')
+            } else{
+                expect(typeof explorers[i].trick).toBe('number')
+            }
+        }
+        
     });
 })
